0e2d8bff6764c2dc9afe3fa56dc025724a2accda
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-04-07 16:30:28 -0500

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -38,7 +38,7 @@ export default function Home() {
                     <h1 className="text-4xl font-bold mb-2">Movies</h1>
                     <p className="text-gray-700">Discover and rate your favorite films</p>
                 </div>
-                <div className="grid py-8 gap-4 sm:gap-6grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                <div className="grid py-8 gap-4 sm:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                     {movies && movies.map((movie) => (
                         <div 
                             key={movie._id} 
